2ba04debd0982da8f8714747e7f5653e4a559b91
tomas.stuina <tomas.stuina@gmail.com>
2023-01-28 17:14:34 +0200

    Make some of the methods expressions shorter (#3)

--- Calculator/Division.cs
+++ Calculator/Division.cs
@@ -7,18 +7,9 @@ public class Division : BinaryOperation<double>
     {
     }
 
-    protected override string? PrintExpression(string? firstExpression, string? secondExpression)
-    {
-        return $"({firstExpression} / {secondExpression})";
-    }
+    protected override string? PrintExpression(string? firstExpression, string? secondExpression) => $"({firstExpression} / {secondExpression})";
 
-    protected override string? PrintExpressionSentence(string? firstSentence, string? secondSentence)
-    {
-        return $"division of {firstSentence} and {secondSentence}";
-    }
+    protected override string? PrintExpressionSentence(string? firstSentence, string? secondSentence) => $"division of {firstSentence} and {secondSentence}";
 
-    protected override double ToResult(double firstValue, double secondValue)
-    {
-        return firstValue / secondValue;
-    }
+    protected override double ToResult(double firstValue, double secondValue) => firstValue / secondValue;
 }
